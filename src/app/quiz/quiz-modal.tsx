fa24bbfcca5889989ee5b9ac2013d9ed656e5695
Shaquilla Johnson <s.evelynjohnson@outlook.com>
2023-07-13 17:12:24 +0100

    quiz-modal

---
+++ src/app/quiz/quiz-modal.tsx
@@ -0,0 +1,11 @@
+export default function QuizModal() {
+  return (
+    <>
+      <div className="z-90 border-2 border-sky-500">
+        <h2 className=" ">Sounds Quiz</h2>
+        <p> Do you know the music news from the last 7 days?</p>
+        {/*  put the quiz here! */}
+      </div>
+    </>
+  );
+}
